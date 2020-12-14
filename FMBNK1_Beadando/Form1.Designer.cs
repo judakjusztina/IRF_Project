b8229cc4631196ede677dc89c9e653bda415fa3d
Jusztina Judák <jusztina.judak@stud.uni-corvinus.hu>
2020-12-14 20:59:41 +0100

    Alap felület elkészítése és lekérdezések.

--- FMBNK1_Beadando/Form1.Designer.cs
+++ FMBNK1_Beadando/Form1.Designer.cs
@@ -29,35 +29,188 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.ExamTB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkboxvizsga = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.checkBoxhianyzas = new System.Windows.Forms.CheckBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(37, 54);
+            this.dataGridView1.Location = new System.Drawing.Point(21, 34);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(551, 384);
+            this.dataGridView1.Size = new System.Drawing.Size(738, 212);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(855, 256);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Mentés (csv)";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(1024, 273);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Törlés";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // ExamTB
+            // 
+            this.ExamTB.Location = new System.Drawing.Point(1022, 61);
+            this.ExamTB.Name = "ExamTB";
+            this.ExamTB.Size = new System.Drawing.Size(120, 22);
+            this.ExamTB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(790, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(226, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Negyedéves vizsga minimum pont:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(790, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Megengedett hiányzások:";
+            // 
+            // checkboxvizsga
+            // 
+            this.checkboxvizsga.AutoSize = true;
+            this.checkboxvizsga.Location = new System.Drawing.Point(1176, 65);
+            this.checkboxvizsga.Name = "checkboxvizsga";
+            this.checkboxvizsga.Size = new System.Drawing.Size(18, 17);
+            this.checkboxvizsga.TabIndex = 9;
+            this.checkboxvizsga.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1024, 29);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 10;
+            // 
+            // checkBoxhianyzas
+            // 
+            this.checkBoxhianyzas.AutoSize = true;
+            this.checkBoxhianyzas.Location = new System.Drawing.Point(1176, 32);
+            this.checkBoxhianyzas.Name = "checkBoxhianyzas";
+            this.checkBoxhianyzas.Size = new System.Drawing.Size(18, 17);
+            this.checkBoxhianyzas.TabIndex = 11;
+            this.checkBoxhianyzas.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(21, 273);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(738, 228);
+            this.dataGridView2.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "A követelményeket nem teljesÍtő hallgatók:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Összes hallgató:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1155, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Aktiválás";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1231, 530);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.checkBoxhianyzas);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkboxvizsga);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ExamTB);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox ExamTB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkboxvizsga;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox checkBoxhianyzas;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- FMBNK1_Beadando/Form1.cs
+++ FMBNK1_Beadando/Form1.cs
@@ -15,6 +15,8 @@ namespace FMBNK1_Beadando
     public partial class Form1 : Form
     {
         BindingList<HallgatoAdat> Adatok = new BindingList<HallgatoAdat>();
+        BindingList<HallgatoAdat> Elegtelen = new BindingList<HallgatoAdat>();
+
        
         public Form1()
         {
@@ -23,25 +25,103 @@ namespace FMBNK1_Beadando
             xmlfeldolgozas();
 
             dataGridView1.DataSource = Adatok;
+            dataGridView2.DataSource = Elegtelen;
         }
         public void xmlfeldolgozas()
         {
+            Adatok.Clear();
+
             XmlDocument xml = new XmlDocument();
             xml.Load("Hallgatok.xml");
 
             foreach (XmlNode node in xml.DocumentElement)
             {
-                var adat = new HallgatoAdat();
 
-                adat.nev = node.FirstChild.InnerText;
-                adat.neptun = node.ChildNodes[1].InnerText;
-                adat.reszvetel = int.Parse(node.ChildNodes[2].InnerText);
-                adat.pontszam = int.Parse(node.ChildNodes[3].InnerText);
-                adat.beadando = node.ChildNodes[4].InnerText;
+                    var adat = new HallgatoAdat();
 
-                Adatok.Add(adat);
+                    adat.nev = node.FirstChild.InnerText;
+                    adat.neptun = node.ChildNodes[1].InnerText;
+                    adat.reszvetel = int.Parse(node.ChildNodes[2].InnerText);
+                    adat.pontszam = int.Parse(node.ChildNodes[3].InnerText);
+                    adat.beadando = node.ChildNodes[4].InnerText;
+
+                    Adatok.Add(adat);
+                
 
             }
         }
+
+        private void NameTB_TextChanged(object sender, EventArgs e)
+        {
+            //dataGridView1.Rows.Clear();
+            
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Elegtelen.Clear();
+
+            XmlDocument xml = new XmlDocument();
+            xml.Load("Hallgatok.xml");
+
+            foreach (XmlNode node in xml.DocumentElement)
+            {
+                
+                if (checkBoxhianyzas.Checked && checkboxvizsga.Checked)
+                {
+                    if (int.Parse(node.ChildNodes[2].InnerText) > numericUpDown1.Value  || int.Parse(node.ChildNodes[3].InnerText) < int.Parse(ExamTB.Text)) 
+                    {
+                        var adat = new HallgatoAdat();
+
+                        adat.nev = node.FirstChild.InnerText;
+                        adat.neptun = node.ChildNodes[1].InnerText;
+                        adat.reszvetel = int.Parse(node.ChildNodes[2].InnerText);
+                        adat.pontszam = int.Parse(node.ChildNodes[3].InnerText);
+                        adat.beadando = node.ChildNodes[4].InnerText;
+
+                        Elegtelen.Add(adat);
+                    }
+                }
+                else if (checkBoxhianyzas.Checked)
+                {
+                    if (int.Parse(node.ChildNodes[2].InnerText) > numericUpDown1.Value)
+                    {
+                        var adat = new HallgatoAdat();
+
+                        adat.nev = node.FirstChild.InnerText;
+                        adat.neptun = node.ChildNodes[1].InnerText;
+                        adat.reszvetel = int.Parse(node.ChildNodes[2].InnerText);
+                        adat.pontszam = int.Parse(node.ChildNodes[3].InnerText);
+                        adat.beadando = node.ChildNodes[4].InnerText;
+
+                        Elegtelen.Add(adat);
+                    }
+                }
+                else if (checkboxvizsga.Checked)
+                {
+                    if (int.Parse(node.ChildNodes[3].InnerText) < int.Parse(ExamTB.Text))
+                    {
+                        var adat = new HallgatoAdat();
+
+                        adat.nev = node.FirstChild.InnerText;
+                        adat.neptun = node.ChildNodes[1].InnerText;
+                        adat.reszvetel = int.Parse(node.ChildNodes[2].InnerText);
+                        adat.pontszam = int.Parse(node.ChildNodes[3].InnerText);
+                        adat.beadando = node.ChildNodes[4].InnerText;
+
+                        Elegtelen.Add(adat);
+                    }
+                }
+
+            }
+
+        }
+
+
     }
 }
